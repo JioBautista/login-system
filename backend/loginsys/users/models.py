904e8a0f15359fe817991b03c6139170b756774a
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-04-24 14:30:20 -0400

    Updates
    - Installed REST framework

--- backend/loginsys/users/models.py
+++ backend/loginsys/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.IntegerField(max_length=8)
