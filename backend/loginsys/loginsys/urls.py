b8850b1571cca13f1be41548b286ad9aaf37dc0e
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-04-25 13:35:36 -0400

    Updates
    - Installed REST framework Token

--- backend/loginsys/loginsys/urls.py
+++ backend/loginsys/loginsys/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from rest_framework.authtoken import views
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("users.urls")),
     path("", include("products.urls")),
+    path("api-token-auth/", views.obtain_auth_token),
 ]
--- backend/loginsys/products/admin.py
+++ backend/loginsys/products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Products
 
 # Register your models here.
+
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+admin.site.register(Products, ProductsAdmin)
